eefa124af913565f8d70a9f0fb7fb925e99ce2af
Jcorrales07 <corralesjoe07@gmail.com>
2022-01-28 13:47:24 -0600

    Atributos, setters y getters

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,65 @@
 public class Animal {
+    private String nombreCientifico;
+    private String nombreComun;
+    private String habitat;
+    private String alimentacion;
+    private String descripAnimal;
+    private String disGeo;
+    private int vida;
+
+    public String getNombreCientifico() {
+        return nombreCientifico;
+    }
+
+    public void setNombreCientifico(String nombreCientifico) {
+        this.nombreCientifico = nombreCientifico;
+    }
+
+    public String getNombreComun() {
+        return nombreComun;
+    }
+
+    public void setNombreComun(String nombreComun) {
+        this.nombreComun = nombreComun;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+
+    public void setAlimentacion(String alimentacion) {
+        this.alimentacion = alimentacion;
+    }
+
+    public String getDescripAnimal() {
+        return descripAnimal;
+    }
+
+    public void setDescripAnimal(String descripAnimal) {
+        this.descripAnimal = descripAnimal;
+    }
+
+    public String getDisGeo() {
+        return disGeo;
+    }
+
+    public void setDisGeo(String disGeo) {
+        this.disGeo = disGeo;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
 }
